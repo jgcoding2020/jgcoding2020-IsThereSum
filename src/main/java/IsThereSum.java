fcfcb98372359d9faecbd31340e610dabb3d100e
Joshua David Gardner <jgardn11@yahoo.com>
2023-03-06 23:39:34 +0000

    Complete

--- src/main/java/IsThereSum.java
+++ src/main/java/IsThereSum.java
@@ -10,6 +10,14 @@ public class IsThereSum {
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
     public boolean check(int[] arr, int target){
+        int testNum = 0;
+        for (int i = 0; i < arr.length - 1; i++){
+            testNum = arr[i];
+            for (int j = 0; j < arr.length; j++){
+                if ((i != j) && (testNum + arr[j]) == target)
+                    return true;
+            }
+        }
         return false;
     }
 }
